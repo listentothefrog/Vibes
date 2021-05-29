0e51a3b69dbb4c61dc3e31f885be18803d2e01ea
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-05-29 18:28:02 -0400

    Implemented github and google oauth

--- vibes-web-app/src/components/WelcomeScreen/WelcomeScreen.tsx
+++ vibes-web-app/src/components/WelcomeScreen/WelcomeScreen.tsx
@@ -1,6 +1,12 @@
 import { Features } from "./Feature/Features"
+import {githubProvider, googleProvider} from "../../firebase/authmethods"
+import socialMediaAuth from "../../firebase/service/auth"
 
 export const WelcomeScreen = () => {
+    const handleOnClick = async (provider:any) => {
+        const res = await socialMediaAuth(provider)
+        console.log(res)
+    }
     return (
         <div className="flex md:flex-col items-center justify-center">
             <div>
@@ -8,7 +14,8 @@ export const WelcomeScreen = () => {
             </div>
             <h3 className="text-2xl font-medium tracking-wide p-2">Connecting you and your people in your life closer.</h3>
             <Features />
-            <button className="bg-blue-600 text-xl m-8 p-2.5 rounded-md font-medium">Login With Github</button>
+            <button onClick={() => handleOnClick(githubProvider)} className="bg-blue-600 text-xl m-8 p-2.5 rounded-md font-medium">Login With Github</button>
+            <button onClick={() => handleOnClick(googleProvider)} className="bg-red-600 text-xl m-2 p-2.5 rounded-md font-medium">Login With Google</button>
         </div>
     )
 }
