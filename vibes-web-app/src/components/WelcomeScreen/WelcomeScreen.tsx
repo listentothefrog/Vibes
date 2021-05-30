7584e0d4b33abbef4c954529756316cf1b220916
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-05-29 20:50:48 -0400

    Removed Github oauth

--- vibes-web-app/src/components/WelcomeScreen/WelcomeScreen.tsx
+++ vibes-web-app/src/components/WelcomeScreen/WelcomeScreen.tsx
@@ -14,7 +14,6 @@ export const WelcomeScreen = () => {
             </div>
             <h3 className="text-2xl font-medium tracking-wide p-2">Connecting you and your people in your life closer.</h3>
             <Features />
-            <button onClick={() => handleOnClick(githubProvider)} className="bg-blue-600 text-xl m-8 p-2.5 rounded-md font-medium">Login With Github</button>
             <button onClick={() => handleOnClick(googleProvider)} className="bg-red-600 text-xl m-2 p-2.5 rounded-md font-medium">Login With Google</button>
         </div>
     )
