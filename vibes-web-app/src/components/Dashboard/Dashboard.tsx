2650b6d44c4d5a936a19b0f5de8b14f287d5d1f8
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-04 20:47:43 -0400

    welcomes user with email

--- vibes-web-app/src/components/Dashboard/Dashboard.tsx
+++ vibes-web-app/src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,17 @@
 import { auth } from "../../firebase/firebaseSetup"
+import { AuthContext } from "../../context/AuthContext"
+import { useContext } from "react"
+
 export const Dashboard = () => {
+    const user = useContext(AuthContext)
+
     const signOut = async () => {
         await auth.signOut()
     }
     return (
         <div>
             <button onClick={signOut}>Sign out</button>
+            <h1>Hello {user?.email}</h1>
         </div>
     )
 }
