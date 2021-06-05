06ffdc317bd0629fd26a7b00fc3082c35d35512e
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-05 19:38:15 -0400

    Update Login UI

--- vibes-web-app/src/components/WelcomeScreen/Login/Login.tsx
+++ vibes-web-app/src/components/WelcomeScreen/Login/Login.tsx
@@ -31,17 +31,64 @@ export const Login = () => {
         }
     }
     return (
-        <div className="input-form-container">
-            <div className="forms-inputs">
-                <label>Email</label>
-                <input className="user-input" type="email" ref={emailRef}placeholder="Email"/>
-                <label>Password</label>
-                <input className="user-input" ref={passwordRef} type="password" placeholder="Password"/>
-                <div className="submit-btn">
-                    <button className="button-sign-in" onClick={signIn}>Sign In</button>
-                    <button className="button-sign-up" onClick={createAccount}>Sign Up</button>
+        <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+            <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-white-900">Sign in to your account</h2>
+            </div>
+            <div className="mt-8 space-y-6">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                <label htmlFor="email-address" className="sr-only">
+                    Email address
+                </label>
+                <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    ref={emailRef}
+                    autoComplete="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-white-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                />
+                </div>
+                <div>
+                <label htmlFor="password" className="sr-only">
+                    Password
+                </label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    ref={passwordRef}
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-white-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                />
                 </div>
             </div>
+
+                <div>
+                    <button onClick={signIn}
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                    Sign in
+                    </button>
+                </div>
+                <div>
+                    <button onClick={createAccount}
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                    Sign Up
+                    </button>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
