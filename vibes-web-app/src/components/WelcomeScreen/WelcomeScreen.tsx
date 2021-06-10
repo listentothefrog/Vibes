531a8ede3ac22057894b4a4adfda0662f5a192b3
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-10 15:35:26 -0400

    Update tailwind

--- vibes-web-app/src/components/WelcomeScreen/WelcomeScreen.tsx
+++ vibes-web-app/src/components/WelcomeScreen/WelcomeScreen.tsx
@@ -3,7 +3,7 @@ import { Login } from "./Login/Login"
 
 export const WelcomeScreen = () => {
     return (
-        <div className="flex md:flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
             <div>
                 <img style={{height: "150px", width: "250px", objectFit: "contain"}} src="https://github.com/listentothefrog/Vibes/blob/staging/logo.png?raw=true" alt="Logo"/>
             </div>
