2665e5b7571946ea89c5d6c5b10accab17050a43
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-09 22:18:10 -0400

    Calling google auth provider

--- vibes-web-app/src/firebase/firebaseSetup.ts
+++ vibes-web-app/src/firebase/firebaseSetup.ts
@@ -1,15 +1,18 @@
-import firebase from "firebase"
-import "firebase/auth"
+import firebase from "firebase";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuOeJAB89wZXrjtmpX1Hha42EfslDss_w",
-    authDomain: "coolvibesgg.firebaseapp.com",
-    projectId: "coolvibesgg",
-    storageBucket: "coolvibesgg.appspot.com",
-    messagingSenderId: "772702090679",
-    appId: "1:772702090679:web:63a0a5c6288c422a0d6432",
-    measurementId: "G-41DTZRZNSN"
-}
+  apiKey: "AIzaSyAuOeJAB89wZXrjtmpX1Hha42EfslDss_w",
+  authDomain: "coolvibesgg.firebaseapp.com",
+  projectId: "coolvibesgg",
+  storageBucket: "coolvibesgg.appspot.com",
+  messagingSenderId: "772702090679",
+  appId: "1:772702090679:web:63a0a5c6288c422a0d6432",
+  measurementId: "G-41DTZRZNSN",
+};
 
-firebase.initializeApp(firebaseConfig)
-export const auth = firebase.auth()
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
