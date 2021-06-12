d4414ca1316077bc5a0fef181ba88a5b70d5bdba
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-11 20:23:07 -0400

    Improved navbar

--- vibes-web-app/src/components/Dashboard/Dashboard.tsx
+++ vibes-web-app/src/components/Dashboard/Dashboard.tsx
@@ -1,22 +1,9 @@
-import { auth } from "../../firebase/firebaseSetup"
-import { AuthContext } from "../../context/AuthContext"
-import { useContext } from "react"
+import { LeftSide } from "./Left-Side/Left-side";
 
 export const Dashboard = () => {
-    const user = useContext(AuthContext)
-
-    const signOut = async () => {
-        await auth.signOut()
-    }
-
-    return (
-        <div className="">
-            <div className="flex flex-row items-center space-between">
-                <img className="m-5 w-200" src="https://github.com/listentothefrog/Vibes/blob/staging/logo.png?raw=true" alt="logo"/>
-                <button onClick={signOut}>Sign Out</button>
-                <img src={user?.photoURL! as string} alt="user_profile" />
-                <h2>{user?.displayName}</h2>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <LeftSide />
+    </div>
+  );
+};
