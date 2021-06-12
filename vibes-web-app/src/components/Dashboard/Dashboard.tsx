4f1b5beae4401bffa49bcf9f72b9d2c9710a2058
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-11 23:11:15 -0400

    Calling chat component

--- vibes-web-app/src/components/Dashboard/Dashboard.tsx
+++ vibes-web-app/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
 import { LeftSide } from "./Left-Side/Left-side";
+import { Chat } from "./Chat/Chat";
 
 export const Dashboard = () => {
   return (
     <div>
-      <LeftSide />
+      <div>
+        <LeftSide />
+      </div>
+      <div>
+        <Chat />
+      </div>
     </div>
   );
 };
