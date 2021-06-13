dfe6eb732d373dd8f38bf6136dc6a8a5b7a0fc41
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-13 14:44:55 -0400

    Change api keys

--- vibes-web-app/src/firebase/firebaseSetup.ts
+++ vibes-web-app/src/firebase/firebaseSetup.ts
@@ -1,18 +1,21 @@
 import firebase from "firebase";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuOeJAB89wZXrjtmpX1Hha42EfslDss_w",
-  authDomain: "coolvibesgg.firebaseapp.com",
-  projectId: "coolvibesgg",
-  storageBucket: "coolvibesgg.appspot.com",
-  messagingSenderId: "772702090679",
-  appId: "1:772702090679:web:63a0a5c6288c422a0d6432",
-  measurementId: "G-41DTZRZNSN",
+  apiKey: "AIzaSyDzrgUE_L8HUNi6UEBRI44EQ1z2syEv-jg",
+  authDomain: "cool-vibes-gg.firebaseapp.com",
+  projectId: "cool-vibes-gg",
+  storageBucket: "cool-vibes-gg.appspot.com",
+  messagingSenderId: "821618941582",
+  appId: "1:821618941582:web:8ebb513ff5bc8687d733df",
+  measurementId: "G-H3JV6DDG15",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
+
+export const firestore = firebase.firestore();
 
 export const provider = new firebase.auth.GoogleAuthProvider();
