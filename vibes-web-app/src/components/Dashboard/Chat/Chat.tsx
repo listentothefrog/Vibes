2b201972182c57e4953c54f0053be9dfabe4ab82
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-13 14:46:08 -0400

    fetches data from database and from ChatMessage component

--- vibes-web-app/src/components/Dashboard/Chat/Chat.tsx
+++ vibes-web-app/src/components/Dashboard/Chat/Chat.tsx
@@ -3,14 +3,25 @@ import { useContext } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { ChatMessage } from "./ChatMessage/ChatMessage";
 import "./Chat.css";
+import { firestore } from "../../../firebase/firebaseSetup";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Message } from "./chat-types";
 
 export const Chat = () => {
   const user = useContext(AuthContext);
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages]: [Message[] | undefined, boolean, Error | undefined] =
+    useCollectionData<Message>(query, { idField: "id" });
   return (
     <div className="flex items-center justify-center w-9/12 mx-auto flex-col">
-      <h1>{user?.displayName}'s Room</h1>
+      <h1 className="text-lg font-bold">
+        The ulitimate programmer and gamer super chat room
+      </h1>
       <div className="main">
-        <ChatMessage />
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
       </div>
       <Input />
     </div>
