212711d49374d9f5ff0e47b438f1f8fe0f46fcee
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-13 16:40:25 -0400

    Better chat ui

--- vibes-web-app/src/components/Dashboard/Chat/ChatMessage/ChatMessage.tsx
+++ vibes-web-app/src/components/Dashboard/Chat/ChatMessage/ChatMessage.tsx
@@ -1,11 +1,46 @@
 import { ChatMessageProps } from "../chat-types";
+import { auth, firestore } from "../../../../firebase/firebaseSetup";
+import { useContext } from "react";
+import { AuthContext } from "../../../../context/AuthContext";
 
 export const ChatMessage = (props: ChatMessageProps) => {
+  const user = useContext(AuthContext);
   const { text, uid, photoURL } = props.message;
+  const messageClass =
+    auth.currentUser != null && uid === auth.currentUser.uid
+      ? "sent"
+      : "received";
+  const deleteClass =
+    auth.currentUser != null && uid === auth.currentUser.uid ? "show" : "hide";
+
+  const deleteMessage = () => {
+    alert("Are you sure you wanna delte your message?");
+    firestore.collection("messages").doc(props.message.id).delete();
+    console.log("Gonna delete the message");
+  };
   return (
-    <div>
-      <img src={photoURL} />
-      <p>{text}</p>
+    <div className={`message ${messageClass}`}>
+      <img className="profile" src={photoURL} alt="profile_picture" />
+      <div className="message_contents">
+        <small className="text-gray-500 font-normal">{user?.displayName}</small>
+        <p className="font-normal">{text}</p>
+      </div>
+      <button onClick={deleteMessage} className={`delete ${deleteClass}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#FF3131"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
