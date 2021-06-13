78529388535db898bae6cf876fd2bc2b5908cd28
Shashank Ellareddy <se7y@Amars-Mac-mini.local>
2021-06-13 14:48:00 -0400

    prevents page from reloading and clears input field when form is submitted

--- vibes-web-app/src/components/Dashboard/Chat/TextingInput/Input.tsx
+++ vibes-web-app/src/components/Dashboard/Chat/TextingInput/Input.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export const Input = () => {
+  const [formValue, setFormValue] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setFormValue("");
+  };
+
   return (
-    <form className="bottom-0 h-10 fixed flex w-full text-base max-w-screen-md">
+    <form
+      onSubmit={handleSubmit}
+      className="bottom-0 h-10 fixed flex w-full text-base max-w-screen-md"
+    >
       <div className="flex items-center">
         <div>
           <input
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
             className="p-2 leading-6 bg-gray-600 rounded-lg border-none outline-none pr-10 text-1xl"
             placeholder="Type your message....."
           />
